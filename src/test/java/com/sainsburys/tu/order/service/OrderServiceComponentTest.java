2b096cb3318d7898a930c215d79bd826fb30c9d7
Naeem Patel <Naeem.Patel@sainsburys.co.uk>
2019-02-05 16:42:05 +0000

    unit test

--- src/test/java/com/sainsburys/tu/order/service/OrderServiceComponentTest.java
+++ src/test/java/com/sainsburys/tu/order/service/OrderServiceComponentTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.test.context.EmbeddedKafka;
@@ -18,40 +19,27 @@ import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-public class OrderServiceUnitTest
+@EmbeddedKafka
+public class OrderServiceComponentTest
 {
-    @InjectMocks
+    @Autowired
     private OrderService orderService;
-
-    @Mock
-    private Order mockOrder;
-
-    @Mock
-    private Page<Order> mockPage;
-
-    @Mock
-    private Pageable pageable;
-
-    @Mock
-    private OrderRepository repository;
 
     @Before
     public void setUp() throws Exception
     {
-        when(repository.findByCode("REAL_ORDER", pageable)).thenReturn(mockPage);
+
     }
 
     @Test
-    public void testFindOrderByCodeForExistingOrderInDB() throws Exception
+    public void testIngestionOfOrdersFromKafkaToDB() throws Exception
     {
-        Page<Order> orders = orderService.findOrderByCode("REAL_ORDER", pageable);
-        assertEquals(mockPage, orders);
+
     }
 
     @Test
-    public void testFindOrderByCodeForNonExistingOrderInDB() throws Exception
+    public void testOrderServiceLookup() throws Exception
     {
-        Page<Order> orders = orderService.findOrderByCode("FAKE_ORDER", pageable);
-        assertNotEquals(mockPage, orders);
+
     }
 }
