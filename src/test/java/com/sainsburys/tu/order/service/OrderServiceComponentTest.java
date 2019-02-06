4caa9e26120119dc35aba9dd071b0c09e80fb894
Naeem Patel <naeem.patel@sainsburys.co.uk>
2019-02-06 11:16:03 +0000

    TUC-1465 Component Test - Kafka Embedded

--- src/test/java/com/sainsburys/tu/order/service/OrderServiceComponentTest.java
+++ src/test/java/com/sainsburys/tu/order/service/OrderServiceComponentTest.java
@@ -2,44 +2,69 @@ package com.sainsburys.tu.order.service;
 
 
 import com.sainsburys.tu.order.persistence.entities.Order;
-import com.sainsburys.tu.order.persistence.repositories.OrderRepository;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.querydsl.QPageRequest;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @EmbeddedKafka
+@SpringBootTest
+@DirtiesContext
 public class OrderServiceComponentTest
 {
+    private static final String ORDER_TOPIC = "col_sainsburys_logistics_delivery_clothing_dispatched";
+    private static final String GROUP_NAME = "tu-order-service";
+
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, ORDER_TOPIC);
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("spring.cloud.stream.kafka.binder.brokers", embeddedKafka.getBrokersAsString());
+        System.setProperty("spring.kafka.consumer.auto-offset-reset", "latest");
+    }
+
     @Autowired
     private OrderService orderService;
 
     @Before
     public void setUp() throws Exception
     {
-
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka);
+        senderProps.put("key.serializer", ByteArraySerializer.class);
+        senderProps.put("value.serializer", ByteArraySerializer.class);
+        DefaultKafkaProducerFactory<byte[], byte[]> pf = new DefaultKafkaProducerFactory<>(senderProps);
+        KafkaTemplate<byte[], byte[]> template = new KafkaTemplate<>(pf, true);
+        template.setDefaultTopic(ORDER_TOPIC);
+        template.sendDefault("{  \"status\": \"collected\", \"code\": \"70010110008100T\" }".getBytes());
+        Thread.sleep(10000);
     }
 
     @Test
-    public void testIngestionOfOrdersFromKafkaToDB() throws Exception
+    public void testIngestionOfOrdersFromKafkaToDBAndOrderLookup() throws Exception
     {
-
-    }
-
-    @Test
-    public void testOrderServiceLookup() throws Exception
-    {
-
+        Page<Order> page = orderService.findOrderByCode("70010110008100T", new PageRequest(0,5));
+        assertNotNull(page);
+        assertNotNull(page.getContent());
+        assertEquals(1, page.getContent().size());
+        assertEquals("70010110008100T", page.getContent().get(0).getCode());
+        assertEquals("collected", page.getContent().get(0).getStatus());
     }
 }
